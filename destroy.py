d76d64e9536ac8b0670afadc00c0e3cd30cce64b
Aaron Aaeng <aaaa9270@colorado.edu>
2019-04-18 12:04:35 -0600

    port

--- destroy.py
+++ destroy.py
@@ -1,7 +1,6 @@
 from contextlib import redirect_stdout
-from flask import Flask
-from flask import request
-from flask import Response
+from flask import Flask, request, Response
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +18,4 @@ def destroy():
         return Response("Your data was improperly formatted.  Please send all data in the post body.", status=400)
 
 if __name__ == '__main__':
-    app.run(debug=False, use_reloader=True)
+    app.run(port=os.environ['PORT'])
